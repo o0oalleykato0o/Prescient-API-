e3b0a5023a8911137e0607f473d9ff3125657ae4
o0oalleykato0o <114154208+o0oalleykato0o@users.noreply.github.com>
2022-09-22 14:14:23 +0200

    Add files via upload
    Prescient-API test suite regression run files added.

---
+++ Prescient Test Suite API - Regression Pack.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Prescient Test Suite API - Regression Pack</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>alison@nml.co.za;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c8da6e6f-1172-4343-a17c-4cb02e4ef17b</testSuiteGuid>
+   <testCaseLink>
+      <guid>01ceee04-2cb0-4028-b5d1-6b9d56c94278</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/POST _GetDailyInvestmentPerformance</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>894cfc42-d067-4abc-a84c-9395ae36852b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Direct Investor - GetUserRoles - Postman</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fa18765f-187a-4919-b842-25803707ab52</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Broker - GetUserRoles - Postman</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>53cbafea-c5bd-4166-9753-6e8f50bade9e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Broker House - GetUserRoles - Postman</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3d2205e5-9db3-4f17-9a6c-453e6f07fbf6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WhiteLabel - GetUserRoles - Postman</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cee8c0bd-3088-45c2-8817-ae4449cdb04c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/System Admin - GetUserRoles - Postman</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d90c7015-2816-4ab8-b493-91a78c155dcb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Multiple Roles - GetUserRoles - Postman</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
